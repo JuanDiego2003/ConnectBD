229d794a2c761fae435acb5fe129b9e1c0f27254
Juan <juan_diegoparedes@ceroca.cat>
2023-12-05 15:55:44 +0100

    Entidades creadas
    Conexión y Entidades creadas

--- juan/src/main/java/Entidades/Equipos.java
+++ juan/src/main/java/Entidades/Equipos.java
@@ -3,4 +3,20 @@ package Entidades;
 public class Equipos {
     private int id_Equipo;
     private String Equipo;
+
+    public int getId_Equipo() {
+        return id_Equipo;
+    }
+
+    public void setId_Equipo(int id_Equipo) {
+        this.id_Equipo = id_Equipo;
+    }
+
+    public String getEquipo() {
+        return Equipo;
+    }
+
+    public void setEquipo(String equipo) {
+        Equipo = equipo;
+    }
 }
--- juan/src/main/java/ObtenerDatosGames.java
+++ juan/src/main/java/ObtenerDatosGames.java
@@ -1,3 +1,5 @@
 public class ObtenerDatosGames {
+    public void leerArchivo(){
 
+    }
 }
