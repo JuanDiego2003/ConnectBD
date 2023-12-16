7d1c2d092077793b42a4dae99f540290fd43d048
Javierbm28 <javier_ericbravo@ceroca.cat>
2023-12-16 19:43:18 +0100

    Insertar, eliminar y actualizar (Genero,Equipo)
    Acabado

--- juan/src/main/java/Entidades/GenerosDAO.java
+++ juan/src/main/java/Entidades/GenerosDAO.java
@@ -29,27 +29,31 @@ public class GenerosDAO {
         return Lista;
     }
 
-    public static boolean InsertarGeneros(List<Generos> generos, Connection connection) {
+    public static boolean InsertarGeneros(List<Generos> equipos, Connection connection) {
         boolean correcto = false;
         int filasAfectadas = 0;
         String consulta = "INSERT INTO generos (id_genero,genero) VALUES(?,?)";
         try (
                 PreparedStatement pstmt = connection.prepareStatement(consulta)) {
-            List<Generos> generoInsertar =new ArrayList<>();
-            int id = 0;
-            for (Generos genero: generos) {
-                if (!generoInsertar.contains(genero)){
-                    genero.setId_genero(id++);
-                    generoInsertar.add(genero);
+            List<Generos> compararExistencia = ConsultarGeneros(connection);
+            int id = compararExistencia.size();
+            for (Generos genero : equipos) {
+                boolean repetido = false;
+                if (genero.getGenero() != null && !genero.getGenero().equalsIgnoreCase("")&&!compararExistencia.isEmpty()) {
+                        for (Generos equip : compararExistencia) {
+                            if (equip.getGenero().replaceAll(" ", "").equalsIgnoreCase(genero.getGenero().replaceAll(" ", ""))) {
+                                repetido = true;
+                            }
+                        }
+                    if (!repetido) {
+                        id++;
+                        pstmt.setInt(1, id);
+                        pstmt.setString(2, genero.getGenero());
+                        filasAfectadas = pstmt.executeUpdate();
+                        correcto = true;
+                    }
                 }
             }
-            for (Generos genero: generoInsertar) {
-                pstmt.setInt(1, genero.getId_genero());
-                pstmt.setString(2, genero.getGenero());
-            }
-
-            correcto = true;
-            filasAfectadas = pstmt.executeUpdate();
         } catch (SQLException e) {
             correcto = false;
             throw new RuntimeException(e);
@@ -60,24 +64,46 @@ public class GenerosDAO {
         return correcto;
     }
 
-    public static void EliminarGames(Generos genero, Connection connection) {
+    public static void EliminarGeneros(int id, Connection connection) {
         boolean correcto = false;
         String consulta = "DELETE FROM generos WHERE id_genero = ?";
         try (
                 PreparedStatement pstmt = connection.prepareStatement(consulta)) {
-                pstmt.setInt(1, genero.getId_genero());
-                pstmt.executeUpdate();
-
-            correcto = true;
-            pstmt.executeUpdate();
+            pstmt.setInt(1, id);
+            if (pstmt.executeUpdate() > 0) {
+                correcto = true;
+            }
         } catch (SQLException e) {
             correcto = false;
             throw new RuntimeException(e);
         }
         if (correcto) {
-            System.out.println("El registro se ha insertado correctamente.");
+            System.out.println("El registro se ha eliminado correctamente.");
         } else {
-            System.out.println("No se pudo insertar el registro.");
+            System.out.println("No se pudo eliminar el registro.");
         }
     }
+
+    public static List<Generos> ActualizarGeneros(Connection connection) {
+        boolean correcto = false;
+        List<Generos> ListUpdate = new ArrayList<>();
+        String consulta = "UPDATE generos SET id_genero=?,genero=?";
+        try (
+                PreparedStatement pstmt = connection.prepareStatement(consulta)) {
+            try (ResultSet resultado = pstmt.executeQuery()) {
+                while (resultado.next()) {
+                    Generos genero = new Generos();
+                    genero.setId_genero(resultado.getInt("id_genero"));
+                    genero.setGenero(resultado.getString("genero"));
+                    ListUpdate.add(genero);
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return ListUpdate;
+    }
+
 }
